5c23eb2c002ac28037d6bcc394e8aca36cb94409
Jirath Eak <jirath.eakthitiworranun@scb.co.th>
2019-04-25 16:46:09 +0700

    setup test

--- Project 04 - Todo/Todo/AppDelegate.swift
+++ Project 04 - Todo/Todo/AppDelegate.swift
@@ -13,6 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    #if AUTOMATION_TEST
+        UIView.setAnimationsEnabled(false)
+    #endif
     // Override point for customization after application launch.
     return true
   }
